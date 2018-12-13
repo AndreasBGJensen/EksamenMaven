97ba60460f0526676f4d7017305eacd12729128a
Andreas <s130016@student.dtu.dk>
2018-12-13 12:31:19 +0100

    GitHub

---
+++ src/main/java/MinAlder.java
@@ -0,0 +1,12 @@
+public class MinAlder {
+
+
+    /**
+     * @auther Andreas:
+     *Denne klasse indeholder en metoder der viser, om en person muligvis har været til eksamen.
+     */
+
+
+
+    
+}
