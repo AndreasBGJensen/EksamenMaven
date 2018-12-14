d42cae3671090b9aab5ffd2ffc2aa87c638e22d4
Andreas <s130016@student.dtu.dk>
2018-12-14 07:45:07 +0100

    Test lavet

--- src/test/java/MinAlderTest.java
+++ src/test/java/MinAlderTest.java
@@ -4,6 +4,21 @@ import static org.junit.Assert.*;
 
 public class MinAlderTest {
 
+    /**
+     * @author Andreas
+     * Formål: At teste metoden, hvorpå det bliver verificeret at man er gammel nok til at gå til eksamen.
+     * @param: Int og String
+     * Fremgangsmåde: 1) Opretter et opjekt med alder og navn. Forventet output. 3) sammenligner om faktisk output er samme som forventet output.
+     * Status: Godkendt
+     */
+    @Test
+   public void gammelNok(){
+    MinAlder person = new MinAlder(17,"Knud");
+    String forventetOutput = "Ja du rigtig nok prøvet at være til eksamen før";
 
 
+
+
+    assertTrue(forventetOutput==person.gammelNok());
+}
 }
